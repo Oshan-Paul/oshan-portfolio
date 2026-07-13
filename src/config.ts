42f1f040b29784ae14c3773c3edf7c64a9226ec9
Oshan-Paul <167123434+Oshan-Paul@users.noreply.github.com>
2026-07-13 22:08:34 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -71,7 +71,7 @@ export const siteConfig = {
       description:
 "Built an IIoT-based predictive maintenance system for industrial motors using ESP32, MQTT, and real-time sensor data (vibration, temperature, and current). Developed anomaly detection and failure prediction models, with edge-to-cloud communication enabling live monitoring, analytics, and alerting through a cloud dashboard." ,
       link: "",
-      skills: ["Edge Computing", "Quest db", "Predictive Maintenance", "Real-Time Data Processing", "Sensors", "Anomaly Detection", "ESP-32"],
+      skills: ["Edge Computing", "Quest db", "Real-Time Data Processing", "Anomaly Detection", "ESP-32"],
     },
     
     {
